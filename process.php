92aaa4102dc04f8bdc373b3d93a8db64c7c85d10
Bossuche <97983574+Bossuche@users.noreply.github.com>
2023-07-19 00:30:53 +0100

    Update process.php

--- process.php
+++ process.php
@@ -1,33 +1,62 @@
-<?php
-    $server = 'localhost'; // 127.0.0.1
-    $username = 'root';
-    $password = 'rootroot';
-    $db = 'csc309';
+<!DOCTYPE html>
+<html>
+<head>
+    <title>CSC 309 DB Class</title>
+</head>
+<body>
+    <h2>Registration Form</h2>
 
-    // Open a new connection
-    $con = new mysqli($server, $username, $password, $db);
+    <!-- Select all users and display in a Table -->
+    <table cellpadding="10" cellspacing="1" border="1">
+        <thead>
+            <tr>
+                <td>SN</td>
+                <td>Last Name</td> 	
+                <td>First Name</td> 	
+                <td>Gender</td>
+                <td>Date of Birth</td> 	
+                <td>Email</td>
+            </tr>
+        </thead>
 
-    // Check connection
-    if ($con->connect_error){
-        die("Connection failed: " . $con->connect_error);
-    }
+        <tbody>
+           <?php
+                //my server variables
+                $server = 'localhost'; // 127.0.0.1
+                $username = 'root';
+                $password = '';
+                $db = 'csc309';
 
-    // Query
-    $firstname = $_POST['firstname'];
-    $lastname = $_POST['lastname'];
-    $email = $_POST['email'];
-
-    $sql = "INSERT INTO users (firstname, lastname, email) 
-    VALUES ('$firstname', '$lastname', '$email')";
-
-    $result = $con->query($sql);
-
-    if ($result === true) {
-        echo 'Record inserted successfully';
-    } else {
-        echo 'Error inserting record: ' . $con->error;
-    }
-
-    // Close connection
-    $con->close();
-?>
+                //my server connection
+                $conn = new mysqli($server, $username, $password, $db);
+            
+            //check server connection
+            if ($conn){
+                //my select query
+                $sql = "SELECT * FROM users";
+                
+                //query result
+                $result = $conn->query($sql);
+                
+                //fetch the data from the result set
+                while ($row = $result->fetch_assoc()) {?>
+                    <tr>
+                        <td><?php echo $row["id"];?></td>
+                        <td><?php echo $row["lastname"];?></td> 	
+                        <td><?php echo $row["firstname"];?></td> 	
+                        <td><?php echo $row["gender"];?></td>
+                        <td><?php echo $row["date_of_birth"];?></td> 	
+                        <td><?php echo $row["email"];?></td>
+                   </tr>
+                
+                 <?php }
+                
+                }
+                // Close connection
+                $conn->close();
+             ?>
+            
+        </tbody>
+    </table>
+  </body>
+</html>
